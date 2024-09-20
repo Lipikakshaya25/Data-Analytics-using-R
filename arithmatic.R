4f4240640efe88960cbbeb2c41c580ca91b47b06
Lipikakshaya25 <lipikakshaya@gmail.com>
2024-09-20 10:03:46 +0530

    Add files via upload

---
+++ arithmatic.R
@@ -0,0 +1,15 @@
+
+num1=readline("enter the number:")
+num2=readline("enter the number:")
+num1=as.integer(num1)
+num2=as.integer(num2)
+sum=num1+num2
+sprintf("sum=%d",sum)
+diff=num1-num2
+sprintf("difference=%d",diff)
+product=num1*num2
+sprintf("product=%d",product)
+divi=num1/num2
+sprintf("division=%d",divi)
+paste("remainder :",num1%%num2)
+paste("power:",num1^num2)
